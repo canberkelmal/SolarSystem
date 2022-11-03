93153baa8e10e1afd8b5f437865ea587a3aef659
Canberk Elmal <elmalcanberk@gmail.com>
2022-11-03 12:28:28 +0300

    SetOrbitSpeeds

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -4,21 +4,24 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-public GameObject MercuryOrbit;
-public  Vector3 MercuryTurn=new Vector3(0,360,0);
-public float MercuryTurnSpeed=0.1f;
+public GameObject SolarSystem;
+public  Vector3 OrbitTurn=new Vector3(0,360,0);
+public float MercuryTurnSpeed=120f;
+public float[] OrbitSpeeds={0, 600f, 200f, 120f, 66.66667f, 10.34483f, 4.067797f, 1.426873f, 0.728155f, 0.483286f};
 
-    // Start is called before the first frame update
+        
     void Start()
     {
+        /* for(int i=1;i<SolarSystem.transform.childCount; i++){
+            SolarSystem.gameObject.transform.GetChild(i).transform.GetChild(0).localScale*= 5;
+        } */
 
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-
-        MercuryOrbit.transform.Rotate(0 , MercuryTurnSpeed*Time.deltaTime , 0 , Space.Self);
-        
+        for(int i=1;i<SolarSystem.transform.childCount; i++){
+            SolarSystem.gameObject.transform.GetChild(i).transform.Rotate(0 , OrbitSpeeds[i]*Time.deltaTime , 0 , Space.Self);
+        }
     }
 }
